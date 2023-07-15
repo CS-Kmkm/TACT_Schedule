b43ad3a3e48b23e2535b72e80e8257495e5f6a26
Koshi <motegi.koshi@gmail.com>
2023-07-15 20:40:25 +0900

    options

--- options.js
+++ options.js
@@ -3,15 +3,11 @@ let saveButton = document.getElementById("save-button");
 
 // Save option
 saveButton.addEventListener('click', function() {
-  // Year input
-  let yearInput = document.getElementById("year-input");
-  let year = yearInput.value;
-
   // Term select
   let termSelect = document.querySelector('input[name="term"]:checked');
-  let term = termSelect.value;
 
-  chrome.storage.sync.set({year: year, term: term}, function() {
-    console.log('Year and term are saved.');
+  let selectedTerm = termSelect.value;
+  chrome.storage.sync.set({selectedTerm: selectedTerm}, function() {
+    console.log('Value is set to ' + selectedTerm);
   });
 });
