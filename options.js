50df7586b9f849d873bcb0dfedbaca54ee82428a
Kmkm <107456014+CS-Kmkm@users.noreply.github.com>
2023-07-18 21:30:19 +0900

    Update options.js

--- options.js
+++ options.js
@@ -3,11 +3,15 @@ let saveButton = document.getElementById("save-button");
 
 // Save option
 saveButton.addEventListener('click', function() {
+  // Year input
+  let yearInput = document.getElementById("year-input");
+  let year = yearInput.value;
+
   // Term select
   let termSelect = document.querySelector('input[name="term"]:checked');
+  let term = termSelect.value;
 
-  let selectedTerm = termSelect.value;
-  chrome.storage.sync.set({selectedTerm: selectedTerm}, function() {
-    console.log('Value is set to ' + selectedTerm);
+  chrome.storage.sync.set({year: year, term: term}, function() {
+    console.log('Year and term are saved.');
   });
 });
